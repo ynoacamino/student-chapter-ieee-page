fb97bd076ad9dc2f3d27d75c7c3aa9278959ee62
Diegoo11 <ynoacamino@gmail.com>
2024-02-25 15:45:18 -0500

    Update color palette in globals.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,84 +1,151 @@
-import type { Config } from "tailwindcss"
-const { fontFamily } = require("tailwindcss/defaultTheme")
+/* eslint-disable global-require */
+import type { Config } from 'tailwindcss';
+
+const { fontFamily } = require('tailwindcss/defaultTheme');
 
 const config = {
-  darkMode: ["class"],
+  darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
-  prefix: "",
+  ],
+  prefix: '',
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
+        '2xl': '1400px',
       },
     },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
         },
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
         },
         destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
         },
         muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
         },
         accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
         },
         popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
         },
         card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
         },
+        'web-orange': {
+          DEFAULT: 'var(--web-orange-500)',
+          50: 'var(--web-orange-50)',
+          100: 'var(--web-orange-100)',
+          200: 'var(--web-orange-200)',
+          300: 'var(--web-orange-300)',
+          400: 'var(--web-orange-400)',
+          500: 'var(--web-orange-500)',
+          600: 'var(--web-orange-600)',
+          700: 'var(--web-orange-700)',
+          800: 'var(--web-orange-800)',
+          900: 'var(--web-orange-900)',
+          950: 'var(--web-orange-950)',
+        },
+        'web-sky': {
+          DEFAULT: 'var(--web-sky-500)',
+          50: 'var(--web-sky-50)',
+          100: 'var(--web-sky-100)',
+          200: 'var(--web-sky-200)',
+          300: 'var(--web-sky-300)',
+          400: 'var(--web-sky-400)',
+          500: 'var(--web-sky-500)',
+          600: 'var(--web-sky-600)',
+          700: 'var(--web-sky-700)',
+          800: 'var(--web-sky-800)',
+          900: 'var(--web-sky-900)',
+          950: 'var(--web-sky-950)',
+        },
+        'web-blue': {
+          DEFAULT: 'var(--web-blue-700)',
+          50: 'var(--web-blue-50)',
+          100: 'var(--web-blue-100)',
+          200: 'var(--web-blue-200)',
+          300: 'var(--web-blue-300)',
+          400: 'var(--web-blue-400)',
+          500: 'var(--web-blue-500)',
+          600: 'var(--web-blue-600)',
+          700: 'var(--web-blue-700)',
+          800: 'var(--web-blue-800)',
+          900: 'var(--web-blue-900)',
+          950: 'var(--web-blue-950)',
+        },
+        'web-gray': {
+          DEFAULT: 'var(--web-gray-500)',
+          50: 'var(--web-gray-50)',
+          100: 'var(--web-gray-100)',
+          200: 'var(--web-gray-200)',
+          300: 'var(--web-gray-300)',
+          400: 'var(--web-gray-400)',
+          500: 'var(--web-gray-500)',
+          600: 'var(--web-gray-600)',
+          700: 'var(--web-gray-700)',
+          800: 'var(--web-gray-800)',
+          900: 'var(--web-gray-900)',
+          950: 'var(--web-gray-950)',
+        },
+        'palete-red': 'hsl(var(--palete-red))',
+        'palete-orange': 'hsl(var(--palete-orange))',
+        'palete-yellow': 'hsl(var(--palete-yellow))',
+        'palete-white-green': 'hsl(var(--palete-white-green))',
+        'palete-green': 'hsl(var(--palete-green))',
+        'palete-purple': 'hsl(var(--palete-purple))',
+        'palete-teal': 'hsl(var(--palete-teal))',
+        'palete-blue': 'hsl(var(--palete-blue))',
+        'palete-cyan': 'hsl(var(--palete-cyan))',
       },
       borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ['var(--font-sans)', ...fontFamily.sans],
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config;
 
-export default config
+export default config;
