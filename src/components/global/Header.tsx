745159409258e3a521b416c0eb4eb2323f007fd4
Diegoo11 <ynoacamino@gmail.com>
2024-02-28 20:30:28 -0500

    Add NextLink component to Header

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,5 +1,6 @@
 import ThemeToggle from '@/components/ui/themeToggle';
 import Link from '@/components/ui/link';
+import NextLink from 'next/link';
 import LanguageToggle from '../ui/languageToggle';
 import IEEETitle from '../logos/IEEETitle';
 
@@ -28,12 +29,12 @@ export default function Header() {
   return (
     <header id="header" className="w-full flex justify-center items-center text-primary sticky top-0 bg-background backdrop-blur-md z-10">
       <div className="w-full max-w-6xl py-2 px-6 flex justify-between items-center">
-        <a href="/">
+        <NextLink href="/">
           <h1 className="font-bold text-xl flex flex-col leading-none text-center">
             <IEEETitle className="w-40 h-auto" />
             <span className="sr-only">IEEE COMPUTER SOCIETY</span>
           </h1>
-        </a>
+        </NextLink>
         <div className="flex gap-2 items-center">
           <nav className="flex items-center justify-start gap-10">
             <div className="md:flex gap-2 hidden font-semibold">
