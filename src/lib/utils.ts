53f43e01bb37c1f9baa99dd3d7950d2a7b4c32f7
Diegoo11 <ynoacamino@gmail.com>
2024-02-29 23:18:39 -0500

    Refactor cn function and add randomStrings utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+function randomStrings(str: string): string[] {
+  const TIMES = 40;
+  const randomChars = '!"#$%&\'()*+,-./0123456789:;<=>?@'.split('');
+
+  const input = str.split('');
+  const result = [];
+
+  for (let i = 0; i < TIMES; i += 1) {
+    for (let j = 0; j < input.length; j += 1) {
+      const random = Math.round(Math.random() * randomChars.length);
+      input[j] = randomChars[random];
+    }
+    result.push(input.join(''));
+  }
+
+  return result;
 }
