ae27b53d8a54ae5be57c3ef8e7cc90e0c0a85158
Diegoo11 <ynoacamino@gmail.com>
2024-03-01 15:06:27 -0500

    Refactor gallery component styles

--- src/components/pages/gallery/Gallery.tsx
+++ src/components/pages/gallery/Gallery.tsx
@@ -1,25 +1,25 @@
 /* eslint-disable react/jsx-pascal-case */
 function Box_1_1() {
   return (
-    <div className="w-full aspect-[1/1] bg-web-gray-100 rounded-md hover:scale-105 transition-transform hover:cursor-pointer hover:shadow-lg" />
+    <div className="w-full aspect-[1/1] bg-web-gray-100 rounded-md transition-shadow hover:cursor-pointer hover:shadow-lg" />
   );
 }
 
 function Box_9_16() {
   return (
-    <div className="w-full aspect-[9/16] bg-web-gray-100 rounded-md hover:scale-105 transition-transform hover:cursor-pointer hover:shadow-lg" />
+    <div className="w-full aspect-[9/16] bg-web-gray-100 rounded-md transition-shadow hover:cursor-pointer hover:shadow-lg" />
   );
 }
 
 function Box_16_9() {
   return (
-    <div className="w-full aspect-[16/9] bg-web-gray-100 rounded-md hover:scale-105 transition-transform hover:cursor-pointer hover:shadow-lg" />
+    <div className="w-full aspect-[16/9] bg-web-gray-100 rounded-md transition-shadow hover:cursor-pointer hover:shadow-lg" />
   );
 }
 
 export default function Gallery() {
   return (
-    <section className="w-full grid grid-cols-2 md:grid-cols-3 gap-6 mb-40">
+    <section className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-0 sm:gap-6 mb-40">
       <div className="flex flex-col gap-6">
         <Box_9_16 />
         <Box_1_1 />
@@ -32,7 +32,7 @@ export default function Gallery() {
         <Box_1_1 />
         <Box_1_1 />
       </div>
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-6 col-span-2 md:col-span-1">
         <Box_1_1 />
         <Box_16_9 />
         <Box_1_1 />
