4e6ed977ae08ecb2a5e1276cc184e9e619941ffc
Diegoo11 <ynoacamino@gmail.com>
2024-03-01 16:11:57 -0500

    Add dynamic animation to Title component

--- src/components/pages/index/Title.tsx
+++ src/components/pages/index/Title.tsx
@@ -1,10 +1,82 @@
+'use client';
+
+import { cn, randomStrings } from '@/lib/utils';
+import {
+  useEffect, useMemo, useState,
+} from 'react';
+
 export default function Title() {
+  const FIRST_WORD = 'STUDENT';
+  const SECOND_WORD = 'BRANCH';
+  const MILISECONDS = 80;
+  const TIMES = 8;
+
+  const [firstWord, setFirstWord] = useState(FIRST_WORD);
+  const [secondWord, setSecondWord] = useState(SECOND_WORD);
+
+  const words1 = useMemo(() => randomStrings({
+    strEnd: FIRST_WORD,
+    strStart: Array.from({ length: FIRST_WORD.length }, () => '0').join(''),
+    times: TIMES,
+  }), []);
+
+  const words2 = useMemo(() => randomStrings({
+    strEnd: SECOND_WORD,
+    strStart: Array.from({ length: SECOND_WORD.length }, () => '0').join(''),
+    times: TIMES + FIRST_WORD.length,
+  }), []);
+
+  const [index1, setIndex1] = useState(0);
+  const [index2, setIndex2] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFirstWord(words1[index1]);
+      setSecondWord(words2[index2]);
+
+      if (index1 < words1.length - 1) setIndex1(index1 + 1);
+      if (index2 < words2.length - 1) setIndex2(index2 + 1);
+
+      if (index1 >= words1.length - 1 && index2 >= words2.length - 1) {
+        clearInterval(interval);
+      }
+    }, MILISECONDS);
+
+    return () => clearInterval(interval);
+  }, [index1, index2, firstWord, secondWord]);
+
   return (
     <div className="flex flex-col w-full my-40 max-w-4xl gap-4">
       <h1 className="text-primary-title font-extrabold flex flex-col text-7xl sm:text-8xl md:text-9xl leading-[0.9] overflow-hidden">
         <span className="text-web-gray text-6xl sm:text-7xl md:text-8xl">IEEE</span>
-        <span>STUDENT</span>
-        <span>BRANCH</span>
+        <span className="flex">
+          {
+            firstWord.split('').map((letter, i) => (
+              <span
+                key={crypto.randomUUID()}
+                className={cn('w-16 sm:w-20 md:w-24 text-center', {
+                  'text-web-gray-200': letter !== FIRST_WORD[i],
+                })}
+              >
+                {letter}
+              </span>
+            ))
+          }
+        </span>
+        <span className="flex">
+          {
+            secondWord.split('').map((letter, i) => (
+              <span
+                key={crypto.randomUUID()}
+                className={cn('w-16 sm:w-20 md:w-24 text-center', {
+                  'text-web-gray-200': letter !== SECOND_WORD[i],
+                })}
+              >
+                {letter}
+              </span>
+            ))
+          }
+        </span>
       </h1>
       <h2 className="text-3xl font-semibold">
         Universidad Nacional de San Agustín de Arequipa
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,20 +5,36 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-function randomStrings(str: string): string[] {
-  const TIMES = 40;
+export function randomStrings({
+  strStart, strEnd, times,
+}: {
+  strStart: string, strEnd: string, times: number
+}): string[] {
+  const TIMES = times;
   const randomChars = '!"#$%&\'()*+,-./0123456789:;<=>?@'.split('');
 
-  const input = str.split('');
+  const input = strStart.split('');
   const result = [];
 
   for (let i = 0; i < TIMES; i += 1) {
+    const randomWord = [];
+
     for (let j = 0; j < input.length; j += 1) {
-      const random = Math.round(Math.random() * randomChars.length);
-      input[j] = randomChars[random];
+      let randomLetter;
+
+      if (TIMES + j - input.length < i) {
+        randomLetter = strEnd[j];
+      } else {
+        randomLetter = randomChars[Math.round(Math.random() * randomChars.length)];
+      }
+
+      randomWord[j] = randomLetter;
     }
-    result.push(input.join(''));
+
+    result.push(randomWord.join(''));
   }
+
+  result.push(strEnd);
 
   return result;
 }
