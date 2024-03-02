a5e3da228148d469c2aacb1e7e64cdac8ac8b835
Diegoo11 <ynoacamino@gmail.com>
2024-03-01 20:31:20 -0500

    Add dialog and update Gallery component

--- src/components/pages/gallery/Gallery.tsx
+++ src/components/pages/gallery/Gallery.tsx
@@ -1,19 +1,44 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { cn } from '@/lib/utils';
+
 /* eslint-disable react/jsx-pascal-case */
-function Box_1_1() {
+function Box({ aspectRatio }: { aspectRatio: '1/1' | '16/9' | '9/16' }) {
   return (
-    <div className="w-full aspect-[1/1] bg-web-gray-100 rounded-md transition-shadow hover:cursor-pointer hover:shadow-lg" />
-  );
-}
-
-function Box_9_16() {
-  return (
-    <div className="w-full aspect-[9/16] bg-web-gray-100 rounded-md transition-shadow hover:cursor-pointer hover:shadow-lg" />
-  );
-}
-
-function Box_16_9() {
-  return (
-    <div className="w-full aspect-[16/9] bg-web-gray-100 rounded-md transition-shadow hover:cursor-pointer hover:shadow-lg" />
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className={cn('w-full aspect-[1/1] bg-web-gray-100 rounded-md transition-shadow hover:cursor-pointer hover:shadow-lg', {
+          'aspect-[16/9]': aspectRatio === '16/9',
+          'aspect-[9/16]': aspectRatio === '9/16',
+          'aspect-[1/1]': aspectRatio === '1/1',
+        })}
+        />
+      </DialogTrigger>
+      <DialogContent className=" w-full">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className={cn('w-full bg-web-gray-100 rounded-md transition-shadow', {
+          'aspect-[16/9]': aspectRatio === '16/9',
+          'aspect-[9/16]': aspectRatio === '9/16',
+          'aspect-[1/1]': aspectRatio === '1/1',
+        })}
+        />
+        <DialogFooter>
+          hola
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
 
@@ -21,22 +46,22 @@ export default function Gallery() {
   return (
     <section className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-0 sm:gap-6 mb-40">
       <div className="flex flex-col gap-6">
-        <Box_9_16 />
-        <Box_1_1 />
-        <Box_1_1 />
-        <Box_16_9 />
+        <Box aspectRatio="9/16" />
+        <Box aspectRatio="1/1" />
+        <Box aspectRatio="1/1" />
+        <Box aspectRatio="16/9" />
       </div>
       <div className="flex flex-col gap-6">
-        <Box_16_9 />
-        <Box_9_16 />
-        <Box_1_1 />
-        <Box_1_1 />
+        <Box aspectRatio="16/9" />
+        <Box aspectRatio="9/16" />
+        <Box aspectRatio="1/1" />
+        <Box aspectRatio="1/1" />
       </div>
       <div className="flex flex-col gap-6 col-span-2 md:col-span-1">
-        <Box_1_1 />
-        <Box_16_9 />
-        <Box_1_1 />
-        <Box_9_16 />
+        <Box aspectRatio="1/1" />
+        <Box aspectRatio="16/9" />
+        <Box aspectRatio="1/1" />
+        <Box aspectRatio="9/16" />
       </div>
     </section>
   );
