4c831cb0a2c03fafeb57db81bf172a1014dd8407
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2024-03-23 11:27:54 -0500

    Refactor theme toggle logic in ThemeToggleMobile component

--- src/components/ui/themeToogleMobile.tsx
+++ src/components/ui/themeToogleMobile.tsx
@@ -3,26 +3,34 @@
 import { useTheme } from 'next-themes';
 import { Sun, Moon, Monitor } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import { useEffect, useState } from 'react';
 import { Button } from './button';
 
 export default function ThemeToggleMobile() {
   const { setTheme, theme } = useTheme();
+  const [realTeam, setRealTheme] = useState('');
+
+  useEffect(() => {
+    setRealTheme(theme || 'light');
+  }, [theme]);
+
   return (
     <div className="flex items-center gap-6 justify-center mt-10">
       <Button
         size="icon"
         variant="outline"
         onClick={() => setTheme('light')}
-        className={cn({ 'bg-primary-title': theme === 'light' })}
+        className={cn({ 'bg-primary-title': realTeam === 'light' })}
       >
         <Sun />
         <span className="sr-only">Tema claro</span>
       </Button>
+
       <Button
         size="icon"
         variant="outline"
         onClick={() => setTheme('dark')}
-        className={cn({ 'bg-primary-title text-white': theme === 'dark' })}
+        className={cn({ 'bg-primary-title text-white': realTeam === 'dark' })}
       >
         <Moon />
         <span className="sr-only">Tema oscuro</span>
@@ -31,7 +39,7 @@ export default function ThemeToggleMobile() {
         size="icon"
         variant="outline"
         onClick={() => setTheme('system')}
-        className={cn({ 'bg-primary-title': theme === 'system' })}
+        className={cn({ 'bg-primary-title': realTeam === 'system' })}
       >
         <Monitor />
         <span className="sr-only">Tema del sistema</span>
