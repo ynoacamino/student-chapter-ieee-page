cd7660f79f945d10eb201160058f6912e01d75aa
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2024-03-23 11:41:41 -0500

    Fix mobile navigation styling

--- src/components/global/NavBarMobile.tsx
+++ src/components/global/NavBarMobile.tsx
@@ -44,14 +44,14 @@ export default function NavBarMobile() {
         <Menu />
       </Button>
       <button
-        className={cn('fixed inset-0 z-20 bg-black/30 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 cursor-default h-screen w-full', { 'left-[-100vh] opacity-0': !isOpen })}
+        className={cn('fixed inset-0 z-20 bg-black/30 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 cursor-default h-screen w-full', { 'left-[-100vw] opacity-0': !isOpen })}
         id="backgroundModal"
         onClick={handleClick}
         type="button"
         aria-hidden
       />
       <nav
-        className={cn('flex flex-col items-center justify-center gap-4 fixed h-screen z-40 w-full max-w-xs bg-one dark:bg-one-dark top-0 left-0 md:hidden transition-all bg-background cursor-default shadow-zinc-500 dark:shadow-zinc-800 shadow-2xl border-r-[1px] border-zinc-300 dark:border-zinc-700', { 'left-[-100vh] opacity-0': !isOpen })}
+        className={cn('flex flex-col items-center justify-center gap-4 fixed h-screen z-40 w-full max-w-xs bg-one dark:bg-one-dark top-0 left-0 md:hidden transition-all bg-background cursor-default shadow-zinc-500 dark:shadow-zinc-800 shadow-2xl border-r-[1px] border-zinc-300 dark:border-zinc-700', { 'left-[-100vw] opacity-0': !isOpen })}
         onClick={(e) => e.stopPropagation()}
       >
         <div className="w-full max-w-40 flex flex-col gap-4">
